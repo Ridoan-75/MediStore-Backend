140e46161437ac66025a22d63c30a8ab0f1e7ca7
Ridoan-75 <ridoan437@gmail.com>
2026-02-02 17:52:49 +0600

    slug.ts

--- src/helpers/generateSlug.ts
+++ src/helpers/generateSlug.ts
@@ -1,8 +1,11 @@
 export const generateSlug = (name: string): string => {
+  if (!name) return "";
+  
   return name
     .toLowerCase()
     .trim()
     .replace(/[^\w\s-]/g, "")
     .replace(/\s+/g, "-")
-    .replace(/-+/g, "-");
+    .replace(/-+/g, "-")
+    .replace(/^-+|-+$/g, "");
 };
