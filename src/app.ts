2a41f75cf8de67c6517c79c14cae08b80fdde197
Ridoan-75 <ridoan437@gmail.com>
2026-02-03 18:35:07 +0600

    error solved

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,10 @@ import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth";
 import categoryRouter from "./modules/category/category.router";
-import medicineRouter from "./modules/medicine/medicine.router"; // ✅ Import
+import medicineRouter from "./modules/medicine/medicine.router";
 import orderRouter from "./modules/order/order.router";
 import reviewRouter from "./modules/review/review.router";
 import userRouter from "./modules/user/user.router";
-
 
 
 const app = express();
@@ -15,15 +14,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.all("/api/auth/*", toNodeHandler(auth));
-app.use("/api/reviews", reviewRouter);
+app.use("/api/auth", toNodeHandler(auth)); 
+
+
 app.use("/api/categories", categoryRouter);
-app.use("/api/medicines", medicineRouter); // ✅ Add this
-app.use("/api/orders", orderRouter)
+app.use("/api/medicines", medicineRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/reviews", reviewRouter);
 app.use("/api/users", userRouter);
 
+
 app.get("/", (req: Request, res: Response) => {
-  res.send("MediStore API is running! 💊");
+  res.json({ 
+    message: "MediStore API is running!"
+  });
 });
 
 export default app;
